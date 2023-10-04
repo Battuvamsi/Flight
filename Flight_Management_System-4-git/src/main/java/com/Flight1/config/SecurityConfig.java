acb75b93fc2f27656633555068c8e2b291f7e4ca
BATTU VAMSI <BATTU VAMSI@LAPTOP-KCVQN81G>
2023-10-04 21:20:12 +0530

    fine

--- Flight_Management_System-4-git/src/main/java/com/Flight1/config/SecurityConfig.java
+++ Flight_Management_System-4-git/src/main/java/com/Flight1/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
     @Bean
-    PasswordEncoder passwordEncoder() {
+   public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 	
